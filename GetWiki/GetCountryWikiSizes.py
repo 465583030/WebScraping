06ace9dc3d4389f33cebca5176ed7b5a114529f2
Ben <b.l.kulcsar@gmail.com>
2016-12-22 13:37:57 +0100

    Update and rename GetWikiSizes.py to GetWiki/GetCountryWikiSizes.py

--- GetWiki/GetCountryWikiSizes.py
+++ GetWiki/GetCountryWikiSizes.py
@@ -1,4 +1,7 @@
 #collecting the size of the wikipedia articles of countries, saving them in excel and saving down the actual article in plain text
+
+#DOES NOT WORK FOR FRANCE - TO BE DONE MANUALLY
+
 import os, requests, bs4, re, openpyxl
 
 #BeautifulSoup gettext function gives text in a messy format, full of special characters. This function is to clean the text.
@@ -9,10 +12,10 @@ def cleanText(sourceText):
     t=re.sub(r"\\x..","-", t) #remove spec characters after the conversion
     t=re.sub(r"\\'","\'", t) #remove \ characters in front of 's
     t=re.sub(r"---References---.*","",t) #remove text that's not part of the article
-    t=re.sub(r".*t TLD","",t) #this slows it down, to be sorted later
+    t=re.sub(r".*t TLD","",t,count=1) #remove noise from the first part
     return t
 
-wikiLinks={0:"nil"} #initialise dictionary that will store links to country wiki pages
+wikiLinks={} #initialise dictionary that will store links to country wiki pages
 
 wikiMain = requests.get('https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)')
 
@@ -21,6 +24,7 @@ mainSoup=bs4.BeautifulSoup(wikiMain.text,"html.parser")
 rows=mainSoup.find_all('tr') #find all rows of the table
 
 i=0 #initialise row counter
+key=0
 
 print("Starting now...\n")
 
@@ -34,15 +38,17 @@ for row in rows:
     
     for cell in cells:
         j+=1
+        print("row no: "+str(i)+", cell no: "+str(j)+", text: "+cell.text)
+        
         if j==1:
-            key=int(cell.text) #1st cell in the row is the country's rank
+            key=int(cell.text) #1st cell is the population rank of the country
         elif j==2:
             link='https://en.wikipedia.org'+cell.find_all('a')[0].get('href') #get link from the 2nd cell
             wikiLinks[int(key)]=link #add country rank and link to the dictionary
         else:
-            continue
+            break
 
-    if i>4: #TO BE REMOVED----------------------------------------
+    if i>234:
         break
 
 print("Links collected:\n")
@@ -56,7 +62,7 @@ print("Excel open\n")
 print("Starting text collection and writing into excel...\n")
 ws['h1']="Wiki size" #first unused column in the excel
 
-for k in range(1,len(wikiLinks)):
+for k in range(1,len(wikiLinks)+1):
     wikiCountry=requests.get(wikiLinks[k]) #get the article
     wikiCountrySoup=bs4.BeautifulSoup(wikiCountry.text,"html.parser")
     wikiCountryText=wikiCountrySoup.get_text() #get the text from the article
